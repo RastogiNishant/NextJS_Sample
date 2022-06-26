8d18e9d9f9129350916a00df656761cff03f2cbf
RastogiNishant <rastoginishant@yahoo.in>
2022-06-26 21:30:06 +0530

    Added comment layout

---
+++ components/input/comments.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import CommentList from 'components/input/comment-list';
+import NewComment from 'components/input/new-comment';
+import classes from 'components/input/comments.module.css';
+
+function Comments(props) {
+  const { eventId } = props;
+
+  const [showComments, setShowComments] = useState(false);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button onClick={toggleCommentsHandler}>
+        {showComments ? 'Hide' : 'Show'} Comments
+      </button>
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+      {showComments && <CommentList />}
+    </section>
+  );
+}
+
+export default Comments;
